4c0a8d297984e64b36016d19cfb810fe3f1b71ac
Sandeep <sk245444@gmail.com>
2026-02-27 14:29:15 +0530

    fix add root route

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,32 @@ const aiRoutes = require("./routes/aiRoutes");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://ai-product-1.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-app.get("/test", (req, res) => {
-  res.send("Backend is working ");
+app.get("/", (req, res) => {
+  res.send("Backend is working");
 });
 
 app.use("/api/products", productRoutes);
